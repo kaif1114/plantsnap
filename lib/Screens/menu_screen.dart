53fd8cd7095c1dddf071d658c6ac27fd5f3e582f
kaifm134 <kaif.m134@gmail.com>
2024-05-11 19:49:57 +0500

    text inside featured plant card adjusted

--- lib/Screens/menu_screen.dart
+++ lib/Screens/menu_screen.dart
@@ -14,41 +14,6 @@ class MenuScreen extends StatefulWidget {
 class MenuScreenState extends State<MenuScreen> {
   final perenualService = PerenualService();
   late Future<Plant?> featuredPlant;
-  // Plant? featuredPlant = Plant(name: "FICUS ELASTICA", images: {"original_url": "https://s3-alpha-sig.figma.com/img/6753/8499/cd36bbec6d3eb630e3d678cd0726fcbc?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CD~3CZeY9ZqTnNzLcfwq-CuXbpBYUCKi7OdzuVZcQ3SVlYmdTpagFSWfloSglRy1a3JpB7BH0Pg6DIpdyjcAMDqHD7rh-2d~VGfTIMUidqi2TFY~RjmHfJUp0qsDeNP9pDjG3rS04SgxbI34XQUR-pOPw01t26PDpGcdZ-sqbt4EWmi2FNh-ADTxC4QHHnz~vSS4UsMAYQ3FSYCliMRjdE~pXehnhIBkt-4vA8KKRmiwJUGc6ggkcjzlEdJxf8mBP692e~tEMFO7G-XP8gFLPhk1WIyMulYdwedFJf5h7LZ1XWJkhwmHkI4zT8kxBI-4OaQFhnPA5J9kEPCKoxbLZw__"});
-
-  Future<void> getFeaturedPlantData() async {
-    print("getFeaturedPlantData function called");
-    // featuredPlant = await perenualService.getRandomPlant();
-    // Plant? ezpz =  await perenualService.getRandomPlant();
-    // setState(() {
-    //   featuredPlant = ezpz;
-    //   if(featuredPlant?.name !=null){
-    //     print(featuredPlant!.name);
-    //   }
-    //   if(featuredPlant?.cycle !=null){
-    //     print(featuredPlant!.cycle);
-    //   }
-    //   if(featuredPlant?.sunlight !=null){
-    //     print(featuredPlant!.sunlight);
-    //   }
-    //   if(featuredPlant?.type !=null){
-    //     print(featuredPlant!.type);
-    //   }
-    //   if(featuredPlant?.description !=null){
-    //     print(featuredPlant!.description);
-    //   }
-    //   if(featuredPlant?.scientificName !=null){
-    //     print(featuredPlant!.scientificName);
-    //   }
-    //   if(featuredPlant?.watering !=null){
-    //     print(featuredPlant!.watering);
-    //   }
-    //   if(featuredPlant?.images?["original_url"] !=null){
-    //     print(featuredPlant!.images!["original_url"]);
-    //   }
-
-    // });
-  }
 
   @override
   void initState() {
@@ -85,10 +50,14 @@ class MenuScreenState extends State<MenuScreen> {
         future: featuredPlant,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator(),);
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
           }
+
           if (snapshot.connectionState == ConnectionState.done) {
             Plant featuredPlantData = snapshot.data!;
+
             return Padding(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               child: Column(
@@ -109,31 +78,45 @@ class MenuScreenState extends State<MenuScreen> {
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(35),
                                 child: SizedBox.fromSize(
-                                    size: const Size.fromRadius(100),
-                                    child: Image.network(
-                                      featuredPlantData.images!["original_url"],
-                                      fit: BoxFit.cover,
-                                    )
-                                    // Image.asset(
-                                    //   'assets/images/house-plant.jpeg',
-                                    //   fit: BoxFit.cover,
-                                    // ),
-                                    ),
+                                  size: const Size.fromRadius(100),
+                                  child: Image.network(
+                                    featuredPlantData.images!["original_url"],
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
                               ),
                             ),
-                            Container(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 10),
+                            SizedBox(
                               width: 160,
-                              child: Text(
-                                featuredPlantData.name.toString(),
-                                style: GoogleFonts.quantico(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w700,
-                                    fontSize: 25,
-                                    fontStyle: FontStyle.italic),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                child: Center(
+                                  child: Text(
+                                    featuredPlantData.name.toString(),
+                                    style: GoogleFonts.quantico(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700,
+                                        fontSize: 20,
+                                        fontStyle: FontStyle.italic),
+                                  ),
+                                ),
                               ),
                             )
+                            // Container(
+                            //   padding:
+                            //       const EdgeInsets.symmetric(horizontal: 10),
+                            //   width: 165,
+                            //   child: Center(
+                            //     child: Text(
+                            //     featuredPlantData.name.toString(),
+                            //     style: GoogleFonts.quantico(
+                            //         color: Colors.white,
+                            //         fontWeight: FontWeight.w700,
+                            //         fontSize: 20,
+                            //         fontStyle: FontStyle.italic),
+                            //   ),
+                            //   )
+                            // )
                           ],
                         ),
                       ),
@@ -168,37 +151,38 @@ class MenuScreenState extends State<MenuScreen> {
                     children: [
                       SizedBox(
                         child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              "All Categories",
-                              style: GoogleFonts.lato(
-                                  fontWeight: FontWeight.w700, fontSize: 18),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 122, 146, 92),
-                                foregroundColor: Colors.white)),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 122, 146, 92),
+                              foregroundColor: Colors.white),
+                          child: Text(
+                            "All Categories",
+                            style: GoogleFonts.lato(
+                                fontWeight: FontWeight.w700, fontSize: 18),
+                          ),
+                        ),
                       ),
                       SizedBox(
                         child: ElevatedButton(
-                            onPressed: () {},
-                            child: Text(
-                              "All Categories",
-                              style: GoogleFonts.lato(
-                                  fontWeight: FontWeight.w700, fontSize: 18),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 122, 146, 92),
-                                foregroundColor: Colors.white)),
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 122, 146, 92),
+                              foregroundColor: Colors.white),
+                          child: Text(
+                            "All Categories",
+                            style: GoogleFonts.lato(
+                                fontWeight: FontWeight.w700, fontSize: 18),
+                          ),
+                        ),
                       ),
                     ],
                   )
                 ],
               ),
             );
-          }
-          else{
+          } else {
             throw Exception("Future Builder Error");
           }
         },
