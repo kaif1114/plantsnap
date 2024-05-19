7dd80b59c07d978db4ef8d8ff5675f25f04d6ccb
kaifm134 <kaif.m134@gmail.com>
2024-05-19 18:11:22 +0500

    Saved plants screen working

--- lib/Screens/saved_plants_screen.dart
+++ lib/Screens/saved_plants_screen.dart
@@ -19,12 +19,14 @@ class SavedPlants extends StatefulWidget {
 
 class SavedPlantsState extends State<SavedPlants> {
   FirestoreService firestoreService = FirestoreService();
-  StreamController<List<Map<String, dynamic>>> savedPlantsStream = StreamController<List<Map<String, dynamic>>>();
+  StreamController<List<Map<String, dynamic>>> savedPlantsStream =
+      StreamController<List<Map<String, dynamic>>>();
 
   Future<void> getSavedPlants() async {
     final userId = FirebaseAuth.instance.currentUser?.uid;
     print("USER ID: $userId");
-    List<Map<String, dynamic>> savedPlants = await firestoreService.getAllDocuments(userId!);
+    List<Map<String, dynamic>> savedPlants =
+        await firestoreService.getAllDocuments(userId!);
     print(savedPlants);
     savedPlantsStream.add(savedPlants);
   }
@@ -44,28 +46,24 @@ class SavedPlantsState extends State<SavedPlants> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<Map<String, dynamic>>>(
-        stream: savedPlantsStream.stream,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-
-          } 
-      
-          else if (snapshot.hasData) {
-            if(snapshot.data![0]["docId"] == "dummy"){
-              return const Center(child: Text("No Saved Plants. Start by scanning a plant!"),);
-            }
-            // Process snapshot data if necessary and update plantList accordingly
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
+      stream: savedPlantsStream.stream,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text("Error: ${snapshot.error}"));
+        } else if (snapshot.hasData) {
+          if (snapshot.data![0]["docId"] == "dummy") {
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  ElevatedButton(
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFF2E7D32), // Dark green color
-                      minimumSize: const Size.fromHeight(50), // Make the button full-width
+                      backgroundColor:
+                          const Color(0xFF2E7D32), // Dark green color
+                      minimumSize: const Size.fromHeight(
+                          50), // Make the button full-width
                     ),
                     onPressed: CameraSnapshotState().handleClick,
                     child: const Text(
@@ -77,65 +75,147 @@ class SavedPlantsState extends State<SavedPlants> {
                       ),
                     ),
                   ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF2E7D32), // Dark green color
+                      minimumSize: const Size.fromHeight(
+                          50), // Make the button full-width
+                    ),
+                    onPressed: (){
+                      getSavedPlants();
+                    },
+                    child: const Text(
+                      'Refresh',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Center(
+                    child: Text("No Saved Plants. Start by scanning a plant!"),
+                  ),
+                ],
+              ),
+            );
+          }
+          // Process snapshot data if necessary and update plantList accordingly
+          return Padding(padding: const EdgeInsets.all(16), child: Column(
+            children: [
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor:
+                        const Color(0xFF2E7D32), // Dark green color
+                    minimumSize:
+                        const Size.fromHeight(50), // Make the button full-width
+                  ),
+                  onPressed: CameraSnapshotState().handleClick,
+                  child: const Text(
+                    'Add Plant',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
-                Expanded( // Use Expanded to ensure ListView.builder gets bounded constraints
-                  child: ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> savedPlant = snapshot.data![index];
-                      return GestureDetector(
-                        onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context){
-                            return PlantDetails(name: savedPlant["plantName"], imageURL: savedPlant["imageURL"], description: savedPlant["plantDescription"], kingdom: savedPlant["plantKingdom"], family: savedPlant["plantFamily"], commonName: savedPlant["commonName"]);
+                const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          const Color(0xFF2E7D32), // Dark green color
+                      minimumSize: const Size.fromHeight(
+                          50), // Make the button full-width
+                    ),
+                    onPressed: (){
+                      getSavedPlants();
+                    },
+                    child: const Text(
+                      'Refresh',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+              Expanded(
+                
+                child: ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    Map<String, dynamic> savedPlant = snapshot.data![index];
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return PlantDetails(
+                                name: savedPlant["plantName"],
+                                imageURL: savedPlant["imageURL"],
+                                description: savedPlant["plantDescription"],
+                                kingdom: savedPlant["plantKingdom"],
+                                family: savedPlant["plantFamily"],
+                                commonName: savedPlant["commonName"]);
                           }));
                         },
                         child: Card(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35)),
-                        margin: const EdgeInsets.all(10),
-                        child: Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(35),
-                                  boxShadow: kElevationToShadow[2],
-                                ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(35),
-                                  child: SizedBox.fromSize(
-                                    size: const Size.fromRadius(60),
-                                    child: Image.network(
-                                      savedPlant["imageURL"],
-                                      fit: BoxFit.cover,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(35)),
+                          margin: const EdgeInsets.all(10),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(35),
+                                    boxShadow: kElevationToShadow[2],
+                                  ),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(35),
+                                    child: SizedBox.fromSize(
+                                      size: const Size.fromRadius(60),
+                                      child: Image.network(
+                                        savedPlant["imageURL"],
+                                        fit: BoxFit.cover,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(width: 16),
-                              Expanded(
-                                child: Text(
-                                  savedPlant["plantName"],
-                                  style: GoogleFonts.lato(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
+                                const SizedBox(width: 16),
+                                Expanded(
+                                  child: Text(
+                                    savedPlant["plantName"],
+                                    style: GoogleFonts.lato(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      )
-                      );
-                    },
-                  ),
+                        ));
+                  },
                 ),
-              ],
-            );
-          } else {
-            return Center(child: Text("No data available"));
-          }
-        },
-      );
+              ),
+            ],
+          ));
+        } else {
+          return Center(child: Text("No data available"));
+        }
+      },
+    );
   }
 }
