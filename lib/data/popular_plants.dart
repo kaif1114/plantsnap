2cc3351416af8bf8d8c39d35e0f654a4f14e1150
kaifm134 <kaif.m134@gmail.com>
2024-05-25 19:50:53 +0500

    more plants added for menu

--- lib/data/popular_plants.dart
+++ lib/data/popular_plants.dart
@@ -14,6 +14,21 @@ List<Map<String, dynamic>> popularPlants = [
     "imageURL": "https://www.mygarden.com/sites/default/files/styles/achor_navigation_s/public/peace-lilly-spathiphyllum-00590833-florapress.jpg?h=badf3774&itok=AVwti6e2",
     "kingdom": "Plant",
     "description": "Spathiphyllum is a genus of about 47 species of monocotyledonous flowering plants in the family Araceae, native to tropical regions of the Americas and southeastern Asia. Certain species of Spathiphyllum are commonly known as spath or peace lilies. They are evergreen herbaceous perennial plants with large leaves 12–65 cm long and 3–25 cm broad. The flowers are produced in a spadix, surrounded by a 10–30 cm long, white, yellowish, or greenish spathe. The plant does not need large amounts of light or water to survive. They are most often grown as houseplants, however they are able to withstand the elements well enough to thrive when planted outdoors in situations that are hot and humid.",
-  }
-  
+  },
+  {
+    "name": "Citrus Limon",
+    "commonName" : "Lemon Bonsai",
+    "family" : "Rutaceae",
+    "imageURL": "https://www.bonsaiempire.com/images/species-photos/bonsai-citrus.jpeg",
+    "kingdom": "Plant",
+    "description": "A bonsai lemon tree is a little ornamental tree that gives you delicious, fresh lemons. It’s a terrific way for amateur gardeners to get some fresh lemons and practice their green thumbs at the same time. In this post, we’ll discuss the merits of keeping a bonsai lemon tree, how to select the best lemon tree variety, and basic care and maintenance procedures.",
+  },
+  {
+    "name": "Aglaonema",
+    "commonName" : "Chinese Evergreen",
+    "family" : "Arums",
+    "imageURL": "https://edsplantshop.com/cdn/shop/products/aglaonema-siam-aurora-red-chinese-evergreen-345035.jpg?v=1705530766",
+    "kingdom": "Plant",
+    "description": "These are evergreen perennials with stems growing erect or decumbent and creeping. Stems that grow along the ground may root at the nodes. There is generally a crown of wide leaf blades which in wild species are often variegated with silver and green coloration. The inflorescence bears unisexual flowers in a spadix, with a short zone of female flowers near the base and a wider zone of male flowers nearer the tip. The fruit is a fleshy berry that ripens red. The fruit is a thin layer covering one large seed.",
+  },
 ];
